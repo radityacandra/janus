0353ca8f1e3a06734876afca39546811cf7835cf
radityacandra <radityacandra@gmail.com>
2025-03-12 20:44:05 +0700

    feat: perform type switch and handle each type to string

--- pkg/plugin/requestadapter/middleware.go
+++ pkg/plugin/requestadapter/middleware.go
@@ -2,9 +2,11 @@ package requestadapter
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
 
 	log "github.com/sirupsen/logrus"
@@ -22,9 +24,9 @@ func NewRequestAdapter(config Config) func(http.Handler) http.Handler {
 			bytes, err := ioutil.ReadAll(r.Body)
 			log.Info(string(bytes))
 			if err == nil {
-				var result map[string]string
+				var result map[string]interface{}
 				if err := json.Unmarshal(bytes, &result); err != nil {
-					result = make(map[string]string)
+					result = make(map[string]interface{})
 				}
 
 				log.WithField("result", result).Info("decoded value")
@@ -33,9 +35,23 @@ func NewRequestAdapter(config Config) func(http.Handler) http.Handler {
 
 				// add form-urlencode
 				for jsonField, formField := range config.Mapping {
-					form.Set(formField, result[jsonField])
+					var strValue string
+					switch v := result[jsonField].(type) {
+					case string:
+						strValue = v
+					case int, int64, float64:
+						strValue = fmt.Sprintf("%v", v)
+					case bool:
+						strValue = strconv.FormatBool(v)
+					default:
+						log.WithField("field", jsonField).WithField("value", v).Warn("Skipping unsupported type")
+						continue
+					}
+
+					form.Set(formField, strValue)
 				}
 
+				log.WithField("encoded_form", form.Encode()).Info("encoded form")
 				formData := form.Encode()
 				r.Body = ioutil.NopCloser(strings.NewReader(formData))
 				r.ContentLength = int64(len(formData))
